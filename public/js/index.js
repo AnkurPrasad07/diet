2242c8d7dde2fbee4bd651e41aa2e70abf13bba6
Ankur <ankurprasad910@gmail.com>
2023-03-31 02:41:53 +0530

    adding hamburger menu

--- public/js/index.js
+++ public/js/index.js
@@ -43,3 +43,10 @@
 
     })
   }
+
+  const nav = document.getElementById("nav")
+  const hamburger = document.getElementById("hamburger-icon")
+
+  hamburger.addEventListener("click", ()=>{
+    nav.classList.toggle("active-toggle")
+  })
