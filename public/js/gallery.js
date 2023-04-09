61b32578f15081ef855c236eeed1e32150dff260
Ankur <ankurprasad910@gmail.com>
2023-04-09 15:54:00 +0530

    Some changes &  building the viewing option of gallery images

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,1 +1,10 @@
-const image = document.querySelectorAll(".gallery-photos img")
+document.querySelectorAll(".gallery-photos img").forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector(".popup-img").style.display = "block"
+        document.querySelector(".popup-img img").src = image.getAttribute('src')
+    }
+})
+
+document.querySelector(".popup-img span").onclick = ()=>{
+    document.querySelector(".popup-img").style.display = "none"
+}
